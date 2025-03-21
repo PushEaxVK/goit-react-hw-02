f2e96272407b211f8e319e0b506a0c3ecc2bf05f
PushEax <pusheax.vk@gmail.com>
2025-03-21 23:27:52 +0200

    add base structure of components

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,21 @@
+import Description from '../Description/Description';
+import Feedback from '../Feedback/Feedback';
+import Options from '../Options/Options';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [state, setState] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
   return (
     <>
-      <h1>Hello, React!</h1>
+      <Description />
+      <Options />
+      <Feedback />
     </>
   );
 }
---
+++ src/components/Options/Options.jsx
@@ -0,0 +1,6 @@
+import css from './Options.module.css';
+
+const Options = () => {
+  return <div className={css.options}>Options</div>;
+};
+export default Options;
